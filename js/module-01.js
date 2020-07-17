57e74de719decd5ce5bc3d0a6680acdb329309c0
Ostap Shevchenko <ostapshev@gmail.com>
2020-07-17 03:03:29 +0300

    add task-02

--- js/module-01.js
+++ js/module-01.js
@@ -9,3 +9,18 @@ console.log(message);
 price = "2000";
 message = `Выбран "${name}", цена за штуку ${price} кредитов`;
 console.log(message);
+
+// ...... Задание #2
+
+const total = 100;
+const ordered = 77;
+let resultMessage;
+
+if (ordered > total) {
+	resultMessage = "На складе недостаточно товаров!";
+} else {
+	resultMessage = "Заказ оформлен, с вами свяжется менеджер";
+}
+console.log(resultMessage);
+
+// ...... Задание #3
