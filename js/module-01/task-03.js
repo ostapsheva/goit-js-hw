2cd9a952317096c904f3c416dfeca9c902692d93
Ostap Shevchenko <ostapshev@gmail.com>
2020-07-17 03:30:14 +0300

    correct task-03

--- js/module-01/task-03.js
+++ js/module-01/task-03.js
@@ -5,10 +5,10 @@ const ADMIN_PASSWORD = "jqueryismyjam";
 let message;
 const password = prompt("Введите пароль");
 
-if (password === ADMIN_PASSWORD) {
-	message = "Добро пожаловать!";
-} else if (password === null) {
+if (password === null) {
 	message = "Отменено пользователем!";
+} else if (password === ADMIN_PASSWORD) {
+	message = "Добро пожаловать!";
 } else {
 	message = "Доступ запрещен, неверный пароль!";
 }
