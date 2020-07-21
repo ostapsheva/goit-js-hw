f8722a2cc7128bdddd337b721f9f0bc3b6b5bb68
Ostap Shevchenko <ostapshev@gmail.com>
2020-07-22 01:07:02 +0300

    Add task-02/module-02

--- js/module-02.js
+++ js/module-02.js
@@ -12,3 +12,17 @@
 // logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
 
 // ...... Задание #2
+
+// const calculateEngravingPrice = function (message, pricePerWord) {
+// 	return message.split(" ").length * pricePerWord;
+// };
+
+// console.log(calculateEngravingPrice("Proin sociis natoque et magnis parturient montes mus", 10)); // 80
+
+// console.log(calculateEngravingPrice("Proin sociis natoque et magnis parturient montes mus", 20)); // 160
+
+// console.log(calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40)); // 240
+
+// console.log(calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)); // 120
+
+// ...... Задание #3
