9ae1383bdd2a5031954ed756c0cb978c1858cf7c
Ostap Shevchenko <ostapshev@gmail.com>
2020-07-22 02:09:53 +0300

    Add task-03/module-2

--- js/module-02.js
+++ js/module-02.js
@@ -26,3 +26,22 @@
 // console.log(calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)); // 120
 
 // ...... Задание #3
+
+// const findLongestWord = function (string) {
+// 	const allWords = string.split(" ");
+// 	const wordsLength = [];
+// 	for (const oneWord of allWords) {
+// 		wordsLength.push(oneWord.length);
+// 	}
+// 	const longestWordIndex = wordsLength.indexOf(Math.max(...wordsLength));
+
+// 	return allWords[longestWordIndex];
+// };
+
+// console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); // 'jumped'
+
+// console.log(findLongestWord("Google do a roll")); // 'Google'
+
+// console.log(findLongestWord("May the force be with you")); // 'force'
+
+// ...... Задание #4
