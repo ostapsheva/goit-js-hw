8f9ba41741f221202a9dc2921d4cc3a68be494df
Ostap Shevchenko <ostapshev@gmail.com>
2020-07-22 00:44:31 +0300

    add task-01/module-02

---
+++ js/module-02.js
@@ -0,0 +1,14 @@
+// ...... Задание #1
+
+// const logItems = function (array) {
+// 	for (let index = 0; index < array.length; index++) {
+// 		const element = array[index];
+// 		console.log(`${index + 1} - ${element}`);
+// 	}
+// };
+
+// logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
+
+// logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
+// ...... Задание #2
