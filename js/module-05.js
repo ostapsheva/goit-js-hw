80a19a13cd3cb29e57d2487fb257ec669f6a663a
Ostap Shevchenko <ostapshev@gmail.com>
2020-07-29 00:00:55 +0300

    add task-02/module-05

--- js/module-05.js
+++ js/module-05.js
@@ -1,26 +1,55 @@
 // ...... Задание #1
 
-const Account = function ({ login, email }) {
-	this.login = login;
-	this.email = email;
-};
+// const Account = function ({ login, email }) {
+// 	this.login = login;
+// 	this.email = email;
+// };
 
-Account.prototype.getInfo = function () {
-	console.log(`Login: ${this.login}, Email: ${this.email}`);
-};
+// Account.prototype.getInfo = function () {
+// 	console.log(`Login: ${this.login}, Email: ${this.email}`);
+// };
 
-console.log(Account.prototype.getInfo); // function
+// console.log(Account.prototype.getInfo); // function
 
-const mango = new Account({
-	login: "Mangozedog",
-	email: "mango@dog.woof",
-});
+// const mango = new Account({
+// 	login: "Mangozedog",
+// 	email: "mango@dog.woof",
+// });
 
-mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+// mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
 
-const poly = new Account({
-	login: "Poly",
-	email: "poly@mail.com",
-});
+// const poly = new Account({
+// 	login: "Poly",
+// 	email: "poly@mail.com",
+// });
 
-poly.getInfo(); // Login: Poly, Email: poly@mail.com
+// poly.getInfo(); // Login: Poly, Email: poly@mail.com
+
+// ...... Задание #2
+
+// class User {
+// 	constructor({ name, age, followers }) {
+// 		this.name = name;
+// 		this.age = age;
+// 		this.followers = followers;
+// 	}
+// 	getInfo() {
+// 		console.log(`User ${this.name} is ${this.age} years old and has ${this.followers} followers`);
+// 	}
+// }
+
+// const mango = new User({
+// 	name: "Mango",
+// 	age: 2,
+// 	followers: 20,
+// });
+
+// mango.getInfo(); // User Mango is 2 years old and has 20 followers
+
+// const poly = new User({
+// 	name: "Poly",
+// 	age: 3,
+// 	followers: 17,
+// });
+
+// poly.getInfo(); // User Poly is 3 years old and has 17 followers
