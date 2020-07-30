9eb1b55e9fc8a2eeef7e5c2d5f0e22d830c2effc
Ostap Shevchenko <ostapshev@gmail.com>
2020-07-31 00:12:12 +0300

    add task-01,02,03,04,05,06,07,08,09,10/module-06

---
+++ js/module-06.js
@@ -0,0 +1,109 @@
+import { default as users } from "./users.js";
+
+// ...... Задание #1
+
+// const getUserNames = users => {
+// 	return users.map(user => user.name);
+// };
+
+// console.log(getUserNames(users));
+
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+// ...... Задание #2
+
+// const getUsersWithEyeColor = (users, color) => {
+// 	return users.filter(user => user.eyeColor === color);
+// };
+
+// console.log(getUsersWithEyeColor(users, "blue"));
+
+// [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+
+// ...... Задание #3
+
+// const getUsersWithGender = (users, gender) => {
+// 	const genderUsers = users.filter(user => user.gender === gender);
+// 	return genderUsers.map(user => user.name);
+// };
+
+// console.log(getUsersWithGender(users, "male"));
+
+// [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+
+// ...... Задание #4
+
+// const getInactiveUsers = users => {
+// 	return users.filter(user => user.isActive === false);
+// };
+
+// console.log(getInactiveUsers(users));
+
+// [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+
+// ...... Задание #5
+
+// const getUserWithEmail = (users, email) => {
+// 	return users.find(user => user.email === email);
+// };
+
+// console.log(getUserWithEmail(users, "shereeanthony@kog.com")); // {объект пользователя Sheree Anthony}
+// console.log(getUserWithEmail(users, "elmahead@omatom.com")); // {объект пользователя Elma Head}
+
+// ...... Задание #6
+
+// const getUsersWithAge = (users, min, max) => {
+// 	return users.filter(user => user.age >= min && user.age <= max);
+// };
+
+// console.log(getUsersWithAge(users, 20, 30));
+// // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+// console.log(getUsersWithAge(users, 30, 40));
+// // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+
+// ...... Задание #7
+
+// const calculateTotalBalance = users => {
+// 	return users.reduce((acc, user) => acc + user.balance, 0);
+// };
+
+// console.log(calculateTotalBalance(users)); // 20916
+
+// ...... Задание #8
+
+// const getUsersWithFriend = (users, friendName) => {
+// 	const filterFriendList = users.filter(user => user.friends.includes(friendName));
+// 	return filterFriendList.map(user => user.name);
+// };
+
+// console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+// console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
+
+// ...... Задание #9
+
+// const getNamesSortedByFriendsCount = users => {
+// 	const sortByFriends = (a, b) => a.friends.length - b.friends.length;
+// 	const sortFriendsList = users.sort(sortByFriends);
+// 	return sortFriendsList.map(user => user.name);
+// };
+
+// console.log(getNamesSortedByFriendsCount(users));
+// // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
+// ...... Задание #10
+
+// const getSortedUniqueSkills = users => {
+// 	return users.reduce((acc, user) => {
+// 		user.skills.forEach(skill => {
+// 			if (!acc.includes(skill)) {
+// 				acc.push(skill);
+// 			}
+// 		});
+// 		acc.sort();
+// 		return acc;
+// 	}, []);
+// };
+
+// console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
