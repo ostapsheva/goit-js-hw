d49cb0f7e38ac324361cc65a9f9ad91e55d72451
Ostap Shevchenko <ostapshev@gmail.com>
2020-08-04 04:52:09 +0300

    Add module-07

--- js/module-07.js
+++ js/module-07.js
@@ -48,3 +48,54 @@
 // const items = images.map(element => {
 // 	gallery.insertAdjacentHTML("beforeend", `<li class="gallery-img"><img src=${element.url} alt=${element.alt}></li>`);
 // });
+
+// ...... Задание #4
+
+// const value = document.querySelector("#value");
+// const decrementButton = document.querySelector('button[data-action="decrement"]');
+// const incrementButton = document.querySelector('button[data-action="increment"]');
+// let counterValue = 0;
+
+// decrementButton.addEventListener("click", e => {
+// 	counterValue -= 1;
+// 	value.textContent = counterValue;
+// });
+// incrementButton.addEventListener("click", e => {
+// 	counterValue += 1;
+// 	value.textContent = counterValue;
+// });
+
+// ...... Задание #5
+
+// const input = document.querySelector("#name-input");
+// const output = document.querySelector("#name-output");
+
+// input.addEventListener("input", e => {
+// 	if (e.target.value === "") {
+// 		output.textContent = "незнакомец";
+// 	} else {
+// 		output.textContent = e.target.value;
+// 	}
+// });
+
+// ...... Задание #6
+
+// const input = document.querySelector("#validation-input");
+// const dataLength = Number(input.getAttribute("data-length"));
+
+// input.addEventListener("change", e => {
+// 	if (e.target.value.length === dataLength) {
+// 		input.classList.add("valid");
+// 		input.classList.remove("invalid");
+// 	} else {
+// 		input.classList.add("invalid");
+// 		input.classList.remove("valid");
+// 	}
+// });
+
+// ...... Задание #7
+
+// const input = document.querySelector("#font-size-control");
+// const span = document.querySelector("#text");
+
+// input.addEventListener("input", e => (span.style.fontSize = `${e.target.value}px`));
